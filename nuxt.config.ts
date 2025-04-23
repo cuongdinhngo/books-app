4390aff15daebc3f8b3d6d8f6b61256cde304923
Cuong Ngo <55419546+cuongdinhngo@users.noreply.github.com>
2025-04-23 22:11:15 +0700

    fix: use static icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,9 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/books-app/',
   },
+  icon: {
+    strategy: 'static'
+  },
   // icon: {
   //   serverBundle: {
   //     collections: ['lucide']
