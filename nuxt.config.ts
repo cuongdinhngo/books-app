edeed3d4607878b8f93bccd8f55a881e91e10e20
Cuong Ngo <55419546+cuongdinhngo@users.noreply.github.com>
2025-04-24 20:51:59 +0700

    chore: ssr disabled

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  ssr: false,
   devtools: { enabled: false },
   modules: ['@nuxt/icon', '@nuxt/ui', '@nuxtjs/supabase', '@vueuse/nuxt'],
   css: ['~/assets/css/main.css'],
