a4a8b879abb42de8222a0e9e531fdcbf86031984
Cuong Ngo <55419546+cuongdinhngo@users.noreply.github.com>
2025-05-02 11:56:55 +0700

    feat: reader's order overdue

--- pages/reader/orders/index.vue
+++ pages/reader/orders/index.vue
@@ -1,13 +1,50 @@
 <template>
-  <h3 class="text-stone-900">Order list</h3>
+  <h3 class="text-stone-900">Historical Orders</h3>
 
   <h3 v-if="status === 'pending'" class="justify-center flex text-stone-900">Loading ...</h3>
   <div v-else>
-    <DataTable
+    <UTable
       v-if="order.count > 0"
       :data="order.data"
       :columns="columns"
-    />
+      class="flex-1"
+    >
+      <template #id-cell="{ row }">
+        <NuxtLink :to="`/reader/orders/${row.original.id}`" class="hover:text-primary-700 cursor-pointer">
+          #{{ row.original.id}}
+        </NuxtLink>
+      </template>
+
+      <template #status-cell="{ row }">
+        <NuxtLink :to="`/reader/orders/${row.original.id}`" class="hover:text-primary-700 cursor-pointer">
+          {{ capitalize(row.original.status) }}
+          <UBadge
+            v-if="row.getValue('status') === ORDER_ITEM_STATUS.BORROWING && new Date().toDateString > row.original.due_date"
+            color="warning" variant="solid"
+          >
+            Overdue
+          </UBadge>
+        </NuxtLink>
+      </template>
+
+      <template #orderItems-cell="{ row }">
+        <NuxtLink :to="`/reader/orders/${row.original.id}`" class="hover:text-primary-700 cursor-pointer">
+          {{ row.getValue('orderItems')[0].count }}
+        </NuxtLink>
+      </template>
+
+      <template #createdAt-cell="{ row }">
+        <NuxtLink :to="`/reader/orders/${row.original.id}`" class="hover:text-primary-700 cursor-pointer">
+          {{ readableDateTime(row.getValue('createdAt')) }}
+        </NuxtLink>
+      </template>
+
+      <template #dueDate-cell="{ row }">
+        <NuxtLink :to="`/reader/orders/${row.original.id}`" class="hover:text-primary-700 cursor-pointer">
+          {{ readableDateTime(row.getValue('dueDate')) }}
+        </NuxtLink>
+      </template>
+    </UTable>
   </div>
 </template>
 
@@ -16,7 +53,8 @@ definePageMeta({
   layout: 'main'
 })
 
-import { NuxtLink } from '#components'; '#components';
+import { ORDER_ITEM_STATUS } from '~/composables/orderItems';
+
 const { index } = useOrders();
 const { userId } = useAuth();
 
@@ -24,6 +62,7 @@ const selectColumns = `
   id,
   status,
   created_at,
+  due_date,
   order_items(count)
 `;
 const {data: order, error, status} = await useAsyncData(
@@ -31,62 +70,31 @@ const {data: order, error, status} = await useAsyncData(
   () => index({ columns: selectColumns, readerId: userId.value }) 
 )
 
+console.log('DATA => ', order);
+
 const columns = [
   {
     accessorKey: 'id',
-    header: '#',
-    cell: ({ row }) => {
-      return h(
-        NuxtLink,
-        {
-          to: `/reader/orders/${row.getValue('id')}`,
-          class: 'hover:text-primary-700 cursor-pointer'
-        },
-        `#${row.getValue('id')}`
-      )
-    }
+    header: 'Order No.'
   },
   {
     accessorKey: 'status',
-    header: 'Status',
-    cell: ({ row }) => {
-      return h(
-        NuxtLink,
-        {
-          to: `/reader/orders/${row.getValue('id')}`,
-          class: 'hover:text-primary-700 cursor-pointer'
-        },
-        capitalize(row.original.status)
-      )
-    }
+    header: 'Status'
   },
   {
     accessorKey: 'order_items',
     header: 'Quantity',
-    cell: ({ row }) => {
-      return h(
-        NuxtLink,
-        {
-          to: `/reader/orders/${row.getValue('id')}`,
-          class: 'hover:text-primary-700 cursor-pointer'
-        },
-        row.getValue('order_items')[0].count
-      )
-    }
+    id: 'orderItems'
   },
   {
     accessorKey: 'created_at',
     header: 'Booked at',
-    cell: ({ row }) => {
-      return h(
-        NuxtLink,
-        {
-          to: `/reader/orders/${row.getValue('id')}`,
-          class: 'hover:text-primary-700 cursor-pointer'
-        },
-        readableDateTime(row.getValue('created_at'))
-      )
-    }
+    id: 'createdAt'
   },
+  {
+    accessorKey: 'due_date',
+    header: 'Due date',
+    id: 'dueDate'
+  }
 ]
 </script>
