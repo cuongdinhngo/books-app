897440a3bd91947ab3343dd78bbbcbb12738db4c
Cuong Ngo <55419546+cuongdinhngo@users.noreply.github.com>
2025-05-03 15:41:44 +0700

    fix: use RPC for admin dashboard

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
         'lucide:ellipsis-vertical', 'lucide:chevrons-left', 'lucide:chevron-right', 'lucide:file-text', 'lucide:swatch-book', 'lucide:cog', 'lucide:chevron-up', 'lucide:x',
         'lucide:user', 'lucide:log-out', 'lucide:search', 'lucide:notebook-pen', 'lucide:chevron-left', 'lucide:chevrons-right', 'lucide:building-2', 'lucide:calendar-cog',
         'lucide:badge-check', 'lucide:notebook-text', 'lucide:trash-2', 'lucide:handshake', 'lucide:clipboard-pen-line', 'lucide:check-check', 'lucide:list', 'lucide:cog',
-        'lucide:bell', 'lucide:book-heart'
+        'lucide:bell', 'lucide:book-heart', 'lucide:arrow-left', 'lucide:arrow-right'
       ]
     }
   },
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -4,23 +4,23 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
         <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Total Categories</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.categoryCounts }}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.total_categories }}</p>
         </div>
         <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Total Publishers</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.publisherCounts}}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.total_publishers }}</p>
         </div>
         <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Total Authors</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.authorCounts }}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.total_authors }}</p>
         </div>
         <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Total Books</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.bookCounts }}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.total_books }}</p>
         </div>
         <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Total Readers</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.readerCounts }}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.total_readers }}</p>
         </div>
     </div>
   </div>
@@ -30,19 +30,19 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
         <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Total Orders</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.orderCounts }}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.total_orders }}</p>
         </div>
         <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Total Borrowed Books</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.borrowedBookCounts}}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.borrowed_orders }}</p>
         </div>
         <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Total Borrowing Books</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.borrowingBookCounts }}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.borrowing_orders }}</p>
         </div>
         <div class="bg-yellow-200 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Overdue Orders</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.overdueOrderCounts }}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.overdue_orders }}</p>
         </div>
     </div>
   </div>
@@ -52,77 +52,40 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
         <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Total Book Items</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.bookItemsCounts }}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.total_book_items }}</p>
         </div>
         <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Total Available Books</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.availableBookCounts }}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.available_books }}</p>
         </div>
         <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
             <p class="text-sm font-medium text-gray-600">Total Lost Books</p>
-            <p class="text-2xl font-semibold text-gray-900">{{ stats?.lostBookCounts }}</p>
+            <p class="text-2xl font-semibold text-gray-900">{{ data?.lost_books }}</p>
         </div>
     </div>
   </div>
+
+  <h3 class="text-stone-800 font-bold mb-1">Top Orders</h3>
+  <UCarousel
+    v-slot="{ item }"
+    loop
+    arrows
+    :items="topOrderBooks"
+    :ui="{
+      item: 'basis-1/4',
+      next: 'end-0',
+      prev: 'start-0'
+    }"
+    class="p-5 bg-primary-50"
+  >
+    <BookRatingItem
+      :book="item"
+    />
+  </UCarousel>
 </template>
 
 <script setup lang="ts">
-import { ORDER_ITEM_STATUS } from '~/composables/orderItems';
-import { BOOK_ITEM_STATUS } from '~/composables/bookItems';
+const { data, error:dashboardError } = await useSupabaseClient().rpc('get_admin_dashboard_counts');
 
-const { index: getCatetoryCounts } = useCategories();
-const { index: getPublisherCounts } = usePublishers();
-const { index: getAuthorCounts } = useAuthors();
-const { index: getBookCounts} = useBooks();
-const { index: getReaderCounts } = useReaders();
-const { index: getOrderCounts } = useOrders();
-const { index: getOrderItemCounts } = useOrderItems();
-const { index: getBookItemsCounts } = useBookItems();
-
-const { data: stats, error } = await useAsyncData('overview-statistics', async () => {
-  const [
-    category,
-    publisher,
-    author,
-    book,
-    reader,
-    order,
-    borrowedOrderItems,
-    borrowingOrderItems,
-    bookItems,
-    availableBooks,
-    lostBooks,
-    overdueOrders
-  ] = await Promise.all([
-    getCatetoryCounts(),
-    getPublisherCounts(),
-    getAuthorCounts(),
-    getBookCounts(),
-    getReaderCounts(),
-    getOrderCounts(),
-    getOrderItemCounts({ status:ORDER_ITEM_STATUS.CLOSED }),
-    getOrderItemCounts({ status:ORDER_ITEM_STATUS.BORROWING }),
-    getBookItemsCounts(),
-    getBookItemsCounts({ status: [BOOK_ITEM_STATUS.OPENING] }),
-    getBookItemsCounts({ status: [BOOK_ITEM_STATUS.LOST] }),
-    getOrderCounts({ status: [ORDER_ITEM_STATUS.BORROWING], isHead: false, isOverdue: true })
-  ]);
-
-  console.log('ORDER OVERDUE => ', overdueOrders);
-
-  return {
-    categoryCounts: category.count,
-    publisherCounts: publisher.count,
-    authorCounts: author.count,
-    bookCounts: book.count,
-    readerCounts: reader.count,
-    orderCounts: order.count,
-    borrowedBookCounts: borrowedOrderItems.count,
-    borrowingBookCounts: borrowingOrderItems.count,
-    bookItemsCounts: bookItems.count,
-    availableBookCounts: availableBooks.count,
-    lostBookCounts: lostBooks.count,
-    overdueOrderCounts: overdueOrders.count
-  }
-});
+const { data:topOrderBooks , error:topOrderError } = await useSupabaseClient().rpc('get_top_ordered_books', { limit_count: 5 });
 </script>
