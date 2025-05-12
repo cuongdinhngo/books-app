65ba7a79b1cb1ac080324a43891f7354d6a7216c
Cuong Ngo <55419546+cuongdinhngo@users.noreply.github.com>
2025-05-12 17:18:45 +0700

    feat: remove wishlist

--- components/Book/Card.vue
+++ components/Book/Card.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="bg-white rounded-lg shadow-lg p-4 max-w-xs w-full">
+  <div class="bg-white rounded-lg shadow-lg p-4 max-w-xs w-full relative group">
+    <UButton
+      icon="lucide:x"
+      size="md"
+      color="primary"
+      variant="solid"
+      class="absolute top-2 right-2 text-white p-2 rounded-full opacity-0 group-hover:opacity-100 transition-opacity"
+      @click="removeFromWishlist"
+      ></UButton>
     <NuxtLink :to="`/book/${book.id}`">
       <img
         :src="book.coverImage"
@@ -9,18 +17,48 @@
     </NuxtLink>
     <div class="flex flex-col">
       <h2 class="text-lg font-semibold text-gray-800 truncate">{{ book.title }}</h2>
-      <p class="text-sm text-gray-600 text-right">Status: {{book.status}}</p>
-      <p class="text-sm text-gray-600 text-right" v-if="book.status === ORDER_ITEM_STATUS.UNAVAILABLE && book.orderItemComment">Reason: {{book.orderItemComment}}</p>
+      <p class="text-sm text-gray-600 text-right capitalize">Status: {{ isAvailable }}</p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ORDER_ITEM_STATUS } from '~/composables/orderItems';
+import { BOOK_COPY_STATUS } from '~/constants/bookCopies';
+
 const props = defineProps({
+  wishlistId: {
+    type: Number,
+    required: true
+  },
   book: {
     type: Object,
     required: true
   }
 });
+
+const { remove } = useWishlists();
+const emit = defineEmits(['refreshWishlists']);
+
+console.log('Book:', props.book);
+
+const isAvailable = computed(() => {
+  return props.book.book_copies.filter((copy) => {
+    return copy.status === BOOK_COPY_STATUS.OPENING;
+  }).length > 0 ? BOOK_COPY_STATUS.AVAILABLE : BOOK_COPY_STATUS.UNAVAILABLE;
+});
+
+async function removeFromWishlist() {
+  console.log(`Removing book with ID ${props.wishlistId} from wishlist`);
+  const result = window.confirm('Are you sure you want to remove this book from your wishlist?');
+  if (!result) return;
+
+  await remove(props.wishlistId)
+    .then(({ error }) => {
+      if (error) throw error;
+
+      emit('refreshWishlists');
+      useToastSuccess('Book removed from wishlist successfully');
+    })
+    .catch((error) => useToastError(error));
+};
 </script>
--- pages/reader/[id].vue
+++ pages/reader/[id].vue
@@ -2,7 +2,7 @@
   <div class="bg-white rounded-lg flex w-full">
     <!-- Left: Profile Photo -->
     <ProfilePhotoBlock
-      :data="data.reader?.data"
+      :data="wishlist.reader?.data"
       :handle-upload-photo="handleUploadPhoto"
     />
     <!-- Right: Information Details -->
@@ -10,32 +10,34 @@
         <div class="space-y-4">
             <div>
                 <span class="text-gray-600">Full Name:</span>
-                <span class="font-bold text-stone-900 ml-1"> {{ data.reader?.data.name }} </span>
+                <span class="font-bold text-stone-900 ml-1"> {{ wishlist.reader?.data.name }} </span>
             </div>
             <div>
                 <span class="text-gray-600">Email:</span>
-                <span class="font-bold text-stone-900 ml-1"> {{ data.reader?.data.email }}</span>
+                <span class="font-bold text-stone-900 ml-1"> {{ wishlist.reader?.data.email }}</span>
             </div>
             <div>
                 <span class="text-gray-600">Address:</span>
-                <span class="font-bold text-stone-900 ml-1"> {{ data.reader?.data.address }}</span>
+                <span class="font-bold text-stone-900 ml-1"> {{ wishlist.reader?.data.address }}</span>
             </div>
             <div>
                 <span class="text-gray-600">Created At:</span>
-                <span class="font-bold text-stone-900 ml-1"> {{ data.reader?.data.created_at ? readableDateTime(data.reader?.data.created_at) : null }}</span>
+                <span class="font-bold text-stone-900 ml-1"> {{ useDateFormat(wishlist.reader?.data.created_at || '', 'MMMM Do, YYYY') }}</span>
             </div>
         </div>
     </div>
   </div>
   <div class="p-4">
-    <h3 class="text-lg font-semibold text-gray-800 mb-2">Your wishlists: {{ wishlists?.length }} books</h3>
+    <h3 class="text-lg font-semibold text-gray-800 mb-2">Your wishlists: {{ wishlist.books.count }} books</h3>
   </div>
 
   <div class="grid grid-cols-1 sm:grid-cols-3 gap-6">
     <BookCard
-      v-for="book in wishlists"
+      v-for="book in wishlist.books.data"
       :key="book.id"
-      :book="book"
+      :book="book.books"
+      :wishlistId="book.id"
+      @refreshWishlists="refresh"
     />
   </div>
 </template>
@@ -43,59 +45,36 @@
 definePageMeta({
   layout: 'main'
 })
+
 import { useRouteParams } from '@vueuse/router';
-import { BOOK_COPY_STATUS } from '~/constants/bookCopies';
 
 const { get, update } = useUsers();
-const { index } = useWishlists();
-const { index:getBookCopies } = useBookCopies();
+const { index:getWishlist } = useWishlists();
 
-const readerId = useRouteParams('id');
+const readerId = useRouteParams('id', null);
 
-const { data, error } = await useAsyncData(
+const columns = `
+  id,book_id,
+  books!inner(
+    id,title,coverImage:cover_image,
+    book_copies(*)
+  )
+`;
+const { data:wishlist, error, refresh } = await useAsyncData(
   `reader/${readerId.value}`,
   async() => {
     const [reader, books] = await Promise.all([
       get(readerId.value),
-      index({ columns: 'id,book_id,books(id,title,coverImage:cover_image)', readerId: readerId.value })
+      getWishlist({ columns: columns, readerId: readerId.value })
     ]);
 
     return { reader, books}
   }
 );
 
-const { data:wishlists, error:wishlistsError } = await useAsyncData(
-  `reader-wishlists-${readerId.value}`,
-  async() => {
-    const ids = data?.value.books.data.map(item => item.book_id);
-    let books = data?.value?.books.data?.map(item => {
-      return {
-        id: item.book_id,
-        title: item.books.title,
-        coverImage: item.books.coverImage,
-        status: BOOK_COPY_STATUS.UNAVAILABLE
-      }
-    });
+console.log('WISHLIST =>', wishlist);
 
-    const { data:bookItems, error } = await getBookCopies({ bookIds: ids, status: [BOOK_COPY_STATUS.OPENING]});
-    if (error) {
-      useToastError(error);
-      return [];
-    }
-
-    if (bookItems?.length > 0) {
-      books.forEach(book => {
-        const availableBook = bookItems.find(ab => ab.book_id === book.id);
-        if (availableBook && availableBook.status === BOOK_COPY_STATUS.OPENING) {
-          book.status = BOOK_COPY_STATUS.AVAILABLE;
-        }
-      });
-    }
-    return books;
-  }
-);
-
-const handleUploadPhoto = async(event) => {
+async function handleUploadPhoto(event) {
   const file = event.target.files[0];
   if (file && file.type.startsWith("image/")) {
     await update(readerId.value, { photo: file })
